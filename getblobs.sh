7bfddb5fe1f23722c21e29ba4b61d2f1cf837342
Pierre Oblin <pierre.oblin@orange.com>
2019-02-03 01:03:42 +0000

    upload blob for terraform

--- getblobs.sh
+++ getblobs.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-TERRAFORM_VERSION="0.11.7"
+TERRAFORM_VERSION="0.11.11"
 wget "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -nv -O ./terraform-${TERRAFORM_VERSION}.zip 
-
+bosh add-blob ./terraform-${TERRAFORM_VERSION}.zip terraform-${TERRAFORM_VERSION}.zip
 # unzip -q /tmp/terraform.zip -d /usr/local/bin
 
