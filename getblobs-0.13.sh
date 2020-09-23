17c070ed1b17ad822c23fd59f443fa329dfacc8f
Pierre Oblin <pierre.oblin@orange.com>
2020-09-23 05:38:57 +0000

    added and uploaded terraform 0.13.3 blob

---
+++ getblobs-0.13.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+TERRAFORM_VERSION="0.13.3"
+wget "https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip" -nv -O ./terraform-${TERRAFORM_VERSION}.zip 
+bosh add-blob ./terraform-${TERRAFORM_VERSION}.zip terraform-${TERRAFORM_VERSION}.zip
+
